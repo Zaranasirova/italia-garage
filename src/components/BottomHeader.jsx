a4e5bac401debd0dd0c734d972fcbd1bd038885f
Zaranasirova <zerifenesirova784@gmail.com>
2024-07-18 01:23:05 +0400

    second commit

--- src/components/BottomHeader.jsx
+++ src/components/BottomHeader.jsx
@@ -1,19 +1,94 @@
 import React from "react";
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 const BottomHeader = () => {
   const location = useLocation();
   const path = location.pathname;
   return (
-    <header   className={`bottomHeader ${path === '/' ? 'active' : ''}`}>
+    <header className="bottomHeader">
       <div className="container">
-        <div   className={`row ${path === '/' ? 'active' : ''}`}>
-          <Link className={`customs ${path === '/' ? 'active' : ''}`}>Customs</Link>
-          <Link  className={`ourGarage ${path === '/' ? 'active' : ''}`}>Our Garage</Link>
-          <Link  className={`marketPlace ${path === '/' ? 'active' : ''}`}>marketPlace</Link>
-          <Link className={`hub ${path === '/' ? 'active' : ''}`}>Hub</Link>
-          <Link to={"/shop"} className={`shop ${path === '/' ? 'active' : ''}`}>Shop</Link>
-          <Link className={`studio ${path === '/' ? 'active' : ''}`}>Studio</Link>
-        </div>
+        <nav>
+          <div className="customs">
+            <Link to={"/"} className="title">Customs</Link>
+            <ul className="customs-dropdown">
+              <li>
+                {" "}
+                <Link to={"/"} className="dropdown-links">
+                  SERVICES
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link to={"/"} className="dropdown-links">
+                  PROJECTS
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="ourGarage">
+            <Link to={"/"} className="title">our Garage</Link>
+            <ul className="garage-dropdown">
+              <li>
+                {" "}
+                <Link to={"/"} className="dropdown-links">
+                  500 Spiaggina
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"} className="dropdown-links">
+                  Vespa 50 Special-e
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link to={"/"} className="dropdown-links">
+                  Spider Duetto Hybrid
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link to={"/"} className="dropdown-links">
+                  Datsun 240z
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link to={"/"} className="dropdown-links">
+                  Panda 4x4
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="marketPlace">
+            <Link to={"/"} className="title">market Place</Link>
+          </div>
+          <div className="hub">
+            <Link to={"/"} className="title">Hub</Link>
+            <ul className="hub-dropdown">
+              <li>
+                {" "}
+                <Link to={"/"} className="dropdown-links">Hub Home</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to={"/"} className="dropdown-links">Garage Culture</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to={"/"}  className="dropdown-links">Garage Customs</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to={"/"}  className="dropdown-links">Studio Projects</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="shop">
+         <Link to={"/shop"} className="title">SHOP</Link>
+          </div>
+          <div className="studio">
+            <Link to={"/"} className="title">STUDIO</Link>
+          </div>
+        </nav>
       </div>
     </header>
   );
--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 import { ReactComponent as Logo } from "../assets/icons/logo.svg";
 import { ReactComponent as Search } from "../assets/icons/search.svg";
 import { ReactComponent as Basket } from "../assets/icons/basket.svg";
@@ -7,45 +7,84 @@ import { ReactComponent as Basket } from "../assets/icons/basket.svg";
 const TopHeader = () => {
   const location = useLocation();
   const path = location.pathname;
+  const formattedPath = path.replace("/", "");
+  const title = `Garage Italia ${formattedPath ? `/ ${formattedPath}` : ""}`;
+  console.log(location.pathname);
   return (
-    <header className={`topHeader ${path === '/' ? 'active' : ''}`}>
+    <header className={`topHeader ${path === "/" ? "active" : ""}`}>
       <div className="container">
         <div className="row">
           <div className="logoName">
-            <Link to={"/"} className={`title ${path === '/' ? 'active' : ''}`}>Garage Italia</Link>
+            <Link to={"/"} className={`title ${path === "/" ? "active" : ""}`}>
+              {title}
+            </Link>
           </div>
           <div className="logo">
             <Link to={"/"}>
-              <Logo className={`logo-icon ${path === '/' ? 'active' : ''}`}/>
+              <Logo className={`logo-icon ${path === "/" ? "active" : ""}`} />
             </Link>
           </div>
-          <div className={`userArea ${path === '/' ? 'active' : ''}`}>
+          <div className={`userArea ${path === "/" ? "active" : ""}`}>
             <div className="social">
-              <span>Social</span>
-
-              {/* <ul className="Dropdown">
-                <li>
-                  <Link>Instagram Customs</Link>
+             <p className="socialTitle">Social</p>
+             <ul className={`dropdownContent ${path === "/" ? "active" : ""}`}>
+                <li className={path === "/" ? "active" : ""}>
+                  <Link
+                    to="https://it-it.facebook.com/garageitaliacustoms/"
+                    className={`socialLink ${path === "/" ? "active" : ""}`} target="_blank"
+                  >
+                    FACEBOOK CUSTOMS
+                  </Link>
                 </li>
-                <li>
-                  <Link>Facebook Customs</Link>
+                <li className={path === "/" ? "active" : ""}>
+                  <Link
+                    to="https://www.instagram.com/garageitalia/"
+                    className={`socialLink ${path === "/" ? "active" : ""}`} target="_blank"
+                  >
+                    INSTAGRAM CUSTOMS
+                  </Link>
                 </li>
-                <li>
-                  <Link>Twitter</Link>
+                <li className={path === "/" ? "active" : ""}>
+                  <Link
+                    to="https://x.com/_GarageItalia_?s=20&t=icRFLrGIItWH5gEfiD-Bcg"
+                    className={`socialLink ${path === "/" ? "active" : ""}`}  target="_blank"
+                  >
+                    TWITTER
+                  </Link>
                 </li>
-                <li>
-                  <Link>Linkedin</Link>
+                <li className={path === "/" ? "active" : ""}>
+                  <Link
+                    to="https://www.linkedin.com/company/garageitalia/?originalSubdomain=it"
+                    className={`socialLink ${path === "/" ? "active" : ""}`} target="_blank"
+                  >
+                    LINKEDIN
+                  </Link>
                 </li>
-                <li>
-                  <Link>Spotify</Link>
+                <li className={path === "/" ? "active" : ""}>
+                  <Link
+                    to="https://open.spotify.com/user/2g1pc6ecmfsjglf5yzu7237n7"
+                    className={`socialLink ${path === "/" ? "active" : ""}`}  target="_blank"
+                  >
+                    SPOTIFY
+                  </Link>
                 </li>
-                <li>
-                  <Link>Instagram Studio</Link>
+                <li  className={path === "/" ? "active" : ""}>
+                  <Link
+                    to="https://www.instagram.com/garageitaliastudio/"
+                    className={`socialLink ${path === "/" ? "active" : ""}`}  target="_blank"
+                  >
+                    INSTAGRAM STUDIO
+                  </Link>
                 </li>
-              </ul> */}
+              </ul>
             </div>
             <div className="contacts">
-              <Link to={"/contacts"} className={`contactTitle ${path === '/' ? 'active' : ''}`}>Contacts</Link>
+              <Link
+                to={"/contacts"}
+                className={`contactTitle ${path === "/" ? "active" : ""}`}
+              >
+                Contacts
+              </Link>
             </div>
             <div className="lang">
               <span>ITA</span>
