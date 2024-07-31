2264bce2aa35a339eec25ef881dd9df147e1a35d
Zaranasirova <zerifenesirova784@gmail.com>
2024-07-31 04:54:56 +0400

    Not Found and Loading commit

---
+++ src/components/Loader/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="hard-loader-overlay">
+    <div className="hard-loader">
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+    </div>
+  </div>
+  )
+}
+
+export default Loading
