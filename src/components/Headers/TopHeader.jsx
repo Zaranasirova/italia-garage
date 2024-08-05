b9c4d5711d439d293d21db2c17e1788a3d6afca2
Zaranasirova <zerifenesirova784@gmail.com>
2024-08-05 18:44:46 +0400

    Header commit

--- src/components/Headers/TopHeader.jsx
+++ src/components/Headers/TopHeader.jsx
@@ -10,44 +10,87 @@ import { ReactComponent as Basket } from "../../assets/icons/basket.svg";
 import { MainContext } from "../../utils/MainContext";
 import BasketCart from "../Basket/BasketCart";
 
-
-
 const TopHeader = () => {
-  const {path, title, cartIsOpen, setCartIsOpen}=useContext(MainContext);
- 
-  useEffect(() => {
-    document.body.style.overflow = cartIsOpen ? "hidden" : "auto";
-  }, [cartIsOpen]);
+  const { path, title, cartIsOpen, setCartIsOpen } = useContext(MainContext);
+
+  // useEffect(() => {
+  //   document.body.style.overflow = cartIsOpen ? "hidden" : "auto";
+  // }, [cartIsOpen]);
   return (
-    <header className={`top-header ${path === "/" ? "active" : ""}`}>
+    // <header className={`top-header ${path === "/" ? "active" : ""}`}>
+    //   <div className="container">
+    //     <div className="row">
+    //       <div className="logo-title">
+    //         <Link to="/" className={`title ${path === "/" ? "active" : ""}`}>{title}</Link>
+    //       </div>
+    //       <div className="logo">
+    //         <Link to={"/"}>
+    //           <Logo className={`logo-icon ${path === "/" ? "active" : ""}`}/>
+    //         </Link>
+    //       </div>
+    //       <div className={`user-area ${path === "/" ? "active" : ""}`}>
+    //         <DropDownList
+    //           triggerText="Social"
+    //           icon={<Dropdown />}
+    //           items={firstDropdown}
+    //           ClassList="dropdown-menu"
+    //           ClassLink="menu-link"
+    //         />
+    //         <Link to="/contacts" className={`contact-page-title ${path === "/" ? "active" : ""}`}>CONTACTS</Link>
+    //         <span className={`lang ${path === "/" ? "active" : ""}`}>ITA</span>
+    //         <FontAwesomeIcon icon={faSearch} className={`search-icon ${path === "/" ? "active" : ""}`} />
+    //         <Basket className={`basket-icon ${path === "/" ? "active" : ""}`} onClick={()=> setCartIsOpen(true)} />
+    //       </div>
+    //     </div>
+    //   </div>
+    //   {cartIsOpen && <BasketCart />}
+    //   {cartIsOpen && (
+    //     <div className="overlay" onClick={()=>setCartIsOpen(false)}></div>
+    //   )}
+    // </header>
+    <header className="top-header">
       <div className="container">
-        <div className="row">
-          <div className="logo-title">
-            <Link to="/" className={`title ${path === "/" ? "active" : ""}`}>{title}</Link>
-          </div>
-          <div className="logo">
-            <Link to={"/"}>
-              <Logo className={`logo-icon ${path === "/" ? "active" : ""}`}/>
+        <div className="header-wrapper row">
+          <div className="header-logo-name">
+            <Link to="/" className={`logo ${path === "/" ? "active" : ""}`}>
+              {title}
             </Link>
           </div>
-          <div className={`user-area ${path === "/" ? "active" : ""}`}>
+          <div className="header-logo">
+            <Link to={"/"}>
+              <Logo className={`logo ${path === "/" ? "active" : ""}`} />
+            </Link>
+          </div>
+          <nav className="nav-bar row">
             <DropDownList
+            
               triggerText="Social"
               icon={<Dropdown />}
               items={firstDropdown}
               ClassList="dropdown-menu"
               ClassLink="menu-link"
             />
-            <Link to="/contacts" className={`contact-page-title ${path === "/" ? "active" : ""}`}>CONTACTS</Link>
+            <Link
+              to="/contacts"
+              className={`contact-page-title ${path === "/" ? "active" : ""}`}
+            >
+              CONTACTS
+            </Link>
             <span className={`lang ${path === "/" ? "active" : ""}`}>ITA</span>
-            <FontAwesomeIcon icon={faSearch} className={`search-icon ${path === "/" ? "active" : ""}`} />
-            <Basket className={`basket-icon ${path === "/" ? "active" : ""}`} onClick={()=> setCartIsOpen(true)} />
-          </div>
+            <FontAwesomeIcon
+              icon={faSearch}
+              className={`search-icon ${path === "/" ? "active" : ""}`}
+            />
+            <Basket
+              className={`basket-icon ${path === "/" ? "active" : ""}`}
+              onClick={() => setCartIsOpen(true)}
+            />
+          </nav>
         </div>
       </div>
       {cartIsOpen && <BasketCart />}
       {cartIsOpen && (
-        <div className="overlay" onClick={()=>setCartIsOpen(false)}></div>
+        <div className="overlay" onClick={() => setCartIsOpen(false)}></div>
       )}
     </header>
   );
