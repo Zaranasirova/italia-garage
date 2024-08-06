7244ef29e803f8d7ac11ccf993516c1a3aa21219
Zaranasirova <zerifenesirova784@gmail.com>
2024-08-07 02:05:32 +0400

    footer changes commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+//REACT ROUTER DOM IMPORT //
 import { Link } from "react-router-dom";
+//ICON IMPORT//
 import { ReactComponent as Logo } from "../../assets/icons/logo.svg";
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <div className="row">
+        <div className="footer-body column">
           <nav className="social-head">
-            <ul className="social-head-list">
+            <ul className="social-head-list row ">
               <li>
                 <Link   to="https://it-it.facebook.com/garageitaliacustoms/" target="_blank" className="social-list-link">
                   Facebook Customs
@@ -43,35 +46,35 @@ const Footer = () => {
             </ul>
           </nav>
           <nav className="social-body">
-            <ul className="social-body-list">
+            <ul className="social-body-list row">
               <li>
-                <ul>
+                <ul className="column">
                   <li>CUSTOMS</li>
                   <li><Link to={"/"} className="list-link">Our Projects</Link></li>
                   <li><Link to={"/"} className="list-link">Our Services</Link></li>
                 </ul>
               </li>
               <li>
-                <ul>
+                <ul className="column">
                   <li>Hub</li>
                   <li><Link to={"/"} className="list-link">HomePage</Link></li>
                   <li><Link to={"/"} className="list-link">ARCHIVE</Link></li>
                 </ul>
               </li>
               <li>
-                <ul>
+                <ul className="column">
                   <li>Shop</li>
                   <li><Link to={"/ShopPage"} className="list-link">Shop All</Link></li>
                   <li><Link to={"/ShopPage"} className="list-link">Sales</Link></li>
                 </ul>
               </li>
               <li>
-                <ul>
+                <ul className="column">
                   <li><Link to={"/"} className="list-link">Studio</Link></li>
                 </ul>
               </li>
               <li>
-                <ul>
+                <ul className="column">
                   <li>Legal</li>
                   <li><Link to={"/"} className="list-link">Privacy Policy</Link></li>
                   <li><Link to={"/"} className="list-link">TERMS & CONDITIONS</Link></li>
@@ -81,7 +84,7 @@ const Footer = () => {
               <li></li>
             </ul>
           </nav>
-          <div className="copy-write">
+          <div className="copy-write row">
             <small>
               GARAGE ITALIA CUSTOMS S.R.L. VIA GALLARATE, 147 - 20151 MILANO,
               ITALIA N. REG. IMPR. MI P.IVA C.F. IT-09555260968
